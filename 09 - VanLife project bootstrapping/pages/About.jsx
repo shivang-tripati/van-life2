df34d3b65c469a3ee8ad4812d09f851e063356c7
Shivang Tripathi <shivangtripathi101@gmail.com>
2023-06-07 17:58:17 +0530

    testing

---
+++ 09 - VanLife project bootstrapping/pages/About.jsx
@@ -0,0 +1,7 @@
+function About() {
+    return (
+      <h1>Hello about, React Router!</h1>
+    )
+  }
+
+export default About;
---
+++ 09 - VanLife project bootstrapping/pages/Home.jsx
@@ -0,0 +1,7 @@
+function Home() {
+    return (
+      <h1>Hello home, React Router!</h1>
+    )
+  }
+
+export default Home;
